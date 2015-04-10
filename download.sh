3072907430a77bebd39ae53df6dab5c63bc9d8e4
Yuki Matsukura <matsubokkuri@gmail.com>
2015-04-10 15:24:55 +0900

    Update download.sh

--- download.sh
+++ download.sh
@@ -7,9 +7,12 @@
 #wget http://downloads.ec-cube.net/src/eccube-2.11.4.tar.gz
 #wget http://downloads.ec-cube.net/src/eccube-2.11.5.tar.gz
 
-wget http://downloads.ec-cube.net/src/eccube-2.12.0.tar.gz
-wget http://downloads.ec-cube.net/src/eccube-2.12.1.tar.gz
-wget http://downloads.ec-cube.net/src/eccube-2.12.2.tar.gz
+# 2.12.3 EC-CUBE plugin used helper function added from 2.12.3
+#wget http://downloads.ec-cube.net/src/eccube-2.12.0.tar.gz
+#wget http://downloads.ec-cube.net/src/eccube-2.12.1.tar.gz
+#wget http://downloads.ec-cube.net/src/eccube-2.12.2.tar.gz
+
+
 wget http://downloads.ec-cube.net/src/eccube-2.12.3.tar.gz
 wget http://downloads.ec-cube.net/src/eccube-2.12.4.tar.gz
 wget http://downloads.ec-cube.net/src/eccube-2.12.5.tar.gz
